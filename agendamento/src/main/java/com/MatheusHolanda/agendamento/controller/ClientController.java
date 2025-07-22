9d34f69d62bad057d39e1fefe3c733ac6593422a
Matheus Passos <matheus.senai2016@gmail.com>
2025-07-22 17:24:27 -0300

    Client Controller

---
+++ agendamento/src/main/java/com/MatheusHolanda/agendamento/controller/ClientController.java
@@ -0,0 +1,60 @@
+package com.MatheusHolanda.agendamento.controller;
+
+import com.MatheusHolanda.agendamento.domain.Client;
+import com.MatheusHolanda.agendamento.repository.ClientRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+
+@RequestMapping("/clients")
+public class ClientController {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @GetMapping
+    public List<Client> findAll() {
+        return clientRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Client> findById(@PathVariable Long id) {
+        return clientRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Client> createClient(@RequestBody @Valid Client client) {
+        Client savedClient = clientRepository.save(client);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedClient);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Client> updateClient(@PathVariable Long id, @RequestBody @Valid Client client) {
+        return clientRepository.findById(id)
+                .map(existing -> {
+                    client.setId(id); // garante que vai atualizar o mesmo id
+                    Client updated = clientRepository.save(client);
+                    return ResponseEntity.ok(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteClient(@PathVariable Long id) {
+        if (!clientRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        clientRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
